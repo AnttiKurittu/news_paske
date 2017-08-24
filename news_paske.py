3bc008d4443cfdff73573732a63212c0c627ee0b
AnttiKurittu <antti.kurittu@gmail.com>
2017-08-24 09:10:57 +0300

    Added the ability to shorten URL's and redirect output to file without color codes.

--- news_paske.py
+++ news_paske.py
@@ -3,12 +3,18 @@
 import subprocess
 import re
 import os
-import urllib
 from bs4 import BeautifulSoup
 import sys
 import argparse
+import urlparse
+import json
+import urllib
+import urllib2
+import requests
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+gapi_key = ''
 
 def get_page_contents(url):
   # Get page contents using curl.
@@ -17,18 +23,29 @@ def get_page_contents(url):
     return False
   proc = subprocess.Popen(["curl", "-s", str(url).rstrip()], stdout=subprocess.PIPE)
   (out, err) = proc.communicate()
-
   try:
-    page = out.encode('utf8')
+    page = out.encode('UTF-8')
   except:
     out = out.decode('latin-1')
-    page = out.encode('utf8')
+    page = out.encode('UTF-8')
 
   if len(page) > 0:
     return page
   else:
     urlerrors[url] = "No content received"
     return False
+
+def shorten_url(url):
+    if gapi_key == '':
+      return "Unable to shorten URL, no API key present."
+    requests.packages.urllib3.disable_warnings()
+    post_url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + gapi_key
+    payload = {'longUrl': url}
+    headers = {'content-type': 'application/json'}
+    payload = json.dumps(payload)
+    r = requests.post(post_url, data=payload, headers=headers)
+    out = json.loads(r.text)
+    return out['id']
 
 def strip_html_tags(data):
   # Clear HTML tags
@@ -39,7 +56,6 @@ def strip_html_tags(data):
 
 #seen_urls_path = os.path.expanduser("~/.seen_urls")
 seen_urls_path = ("/ncsc-fi/cases/news_paske_seen/.seen_urls")
-
 
 try:
   seen_urls = open(seen_urls_path, "r+")
@@ -66,6 +82,27 @@ parser.add_argument("-p",
                     type=int,
                     help="Define minimum length for opening paragraph. Default 100 characters.")
 
+parser.add_argument("-o",
+                    "--outfile",
+                    metavar='filename',
+                    type=str,
+                    help="Output to file instead of stdout")
+
+parser.add_argument("-i",
+                    "--increment",
+                    help="Increment story index letter for already seen urls - useful if adding to a list that already has added URLs.",
+                    action="store_true")
+
+parser.add_argument("-m",
+                    "--minify",
+                    help="Shorten URLs using the Google URL shortener service.",
+                    action="store_true")
+
+parser.add_argument("-q",
+                    "--quiet",
+                    help="Suppress error messages",
+                    action="store_true")
+
 parser.add_argument("-d",
                     "--debug",
                     help="Debug mode - do not store or check seen URLs",
@@ -73,7 +110,28 @@ parser.add_argument("-d",
 
 arg = parser.parse_args()
 
+if arg.outfile:
+    class c:
+
+        D = HDR = B = G = Y = R = END = BOLD = UL = ''
+else:
+    class c:
+        HDR = '\033[96m'
+        B = '\033[94m'
+        Y = '\033[93m'
+        G = '\033[92m'
+        R = '\033[91m'
+        D = '\033[90m'
+        END = '\033[0m'
+        BOLD = '\033[1m'
+        UL = '\033[4m'
+
 urlerrors = {}
+
+if arg.outfile:
+  print "Saving to file %s, please wait..." % arg.outfile,
+  sys.stdout.flush()
+  sys.stdout = open(arg.outfile, 'w')
 
 # Start from a different letter if appending to an existing list of news
 if arg.start:
@@ -99,6 +157,8 @@ for url in sys.stdin:
     url = url[0] + "\r\n"
   if url in seen_urls and arg.debug != True:
     urlerrors[url] = "Already seen"
+    if arg.increment:
+      charcode += 1
     continue
   if str(url)[0:1] == "+":
     add_as_comment = True
@@ -112,9 +172,12 @@ for url in sys.stdin:
     page = get_page_contents(url)
     if page == False:
       continue
-    charcode += 1
     soup = BeautifulSoup(page)
-    title = soup.title.string
+    try:
+      title = soup.title.string
+    except AttributeError:
+      urlerrors[url] = "No page title found"
+      continue
     paragraphs = soup.findAll('p')
     opening_paragraph = 0
     blurb = strip_html_tags(str(paragraphs[0]))
@@ -128,13 +191,22 @@ for url in sys.stdin:
         blurb = False
         urlerrors[url] = "No quotable paragraphs found (min %s chars)" % blurb_min_length
         break
-    print( '\r\n\033[4m\033[92m' + url.strip() + '\033[0m')
-    print( '\033[93m' + chr(charcode) + ':|' + '\033[0m\033[1m' + title.strip() + '\033[0m').replace('\n', '').replace('\r', '')
+    charcode += 1
+    if arg.minify:
+      short_url = " (" + shorten_url(url.strip()) + ")"
+    else:
+      short_url = ""
+    print( '\r\n' + c.UL + c.G + url.strip() + c.END)
+    print( c.Y + chr(charcode) + ':|' + c.END + c.BOLD + title.strip() + c.END).replace('\n', '').replace('\r', '')
     if blurb != False:
-      print( '\033[93m' + chr(charcode) + ':' + '\033[0m' + blurb.strip().replace('\n', '').replace('\r', ''))
-print("")
-if urlerrors:
-  print "\033[91mEncountered %s error(s) while processing:" % len(urlerrors)
-  for url, error in urlerrors.iteritems():
-    print('\033[0m * %s: %s' % (error, url.strip()))
+      print( c.Y + chr(charcode) + ':' + c.END + blurb.strip().replace('\n', '').replace('\r', '') + short_url)
 
+print("")
+if urlerrors and arg.quiet != True:
+  print c.R + "Encountered %s error(s) while processing:" % len(urlerrors)
+  for url, error in urlerrors.iteritems():
+    print(c.END + ' * %s: %s' % (error, url.strip()))
+
+if arg.outfile:
+  sys.stdout = sys.__stdout__
+  print "Done."
